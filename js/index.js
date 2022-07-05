687b74f84c80adcbb39b8a37673fb55c4d793b68
tesfaye eshetie <tesfayeeshetie19@gmail.com>
2022-07-05 16:50:41 -0400

    add @media query and random optimization function

--- js/index.js
+++ js/index.js
@@ -2,24 +2,22 @@ const baseURL = 'https://picsum.photos/v2/list?limit=100';
 
 const Container = document.querySelector('.container');
 
-async function fetchPicsumPhotos() {
+const fetchPicsumPhotos = async () => {
   try {
     const response = await fetch(baseURL);
     const photos = await response.json();
-    photos
-      .filter((photo, index) => index === Math.floor(Math.random() * 100))
-      .forEach((photo) => {
-        const photoUI = `
+    const randomPhoto = await photos[Math.floor(Math.random() * photos.length)];
+    const photoUI = `
           <div class="card">
-            <img src="${photo.download_url}" alter="picsum photo" loading="lazy">
-            <h2><span>Author name :</span> ${photo.author}</h2>
-            <h4>Photo id : ${photo.id}</h4>
+            <img src="${randomPhoto.download_url}" alter="picsum photo" loading="lazy">
+            <h2><span>Author name :</span> ${randomPhoto.author}</h2>
+            <h4>Photo id : ${randomPhoto.id}</h4>
           </div>
         `;
-        Container.innerHTML = photoUI;
-      });
+    Container.innerHTML = photoUI;
   } catch (error) {
     console.log(error.message);
   }
-}
+};
+
 fetchPicsumPhotos();
