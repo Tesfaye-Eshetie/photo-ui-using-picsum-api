58bb7a21e93085ee0e95ec8507b1a3330fbb6060
tesfaye eshetie <tesfayeeshetie19@gmail.com>
2022-07-07 16:58:24 -0400

    Added button to render random picture

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const baseURL = 'https://picsum.photos/v2/list?limit=100';
 
-const Container = document.querySelector('.container');
+const Card = document.querySelector('.card');
+const Button = document.querySelector('#button');
 
 const fetchPicsumPhotos = async () => {
   try {
@@ -8,16 +9,14 @@ const fetchPicsumPhotos = async () => {
     const photos = await response.json();
     const randomPhoto = await photos[Math.floor(Math.random() * photos.length)];
     const photoUI = `
-          <div class="card">
             <img src="${randomPhoto.download_url}" alter="picsum photo" loading="lazy">
             <h2><span>Author name :</span> ${randomPhoto.author}</h2>
             <h4>Photo id : ${randomPhoto.id}</h4>
-          </div>
         `;
-    Container.innerHTML = photoUI;
+    Card.innerHTML = photoUI;
   } catch (error) {
     console.log(error.message);
   }
 };
-
-fetchPicsumPhotos();
+window.addEventListener('load', fetchPicsumPhotos);
+Button.addEventListener('click', fetchPicsumPhotos);
